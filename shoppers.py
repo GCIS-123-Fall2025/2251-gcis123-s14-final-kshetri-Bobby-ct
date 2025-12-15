d11d2b94e903fa1c0d5ea765c699d17395174f35
Bobby Genova (RIT Student) <bsg1878@rit.edu>
2025-12-15 09:18:37 -0500

    question one WIP

--- shoppers.py
+++ shoppers.py
@@ -18,11 +18,55 @@ Write down the manual test by creating at least two items.
 """
 
 class Item:
-    pass # please replace with your solution
+  __slots__ = ['__item_code', '__item_name', '__item_price'] 
+  def __init__(self, code, name, price):
+    self.__item_code = code
+    self.__item_name = name
+    self.__item_price = int(price)  # Really should be float, but the example states simply "24", not "24.0" or "24.00", so using int to match that
+
+  # Accessors
+  def get_code(self):
+    return self.__item_code
+  def get_name(self):
+    return self.__item_name
+  def get_price(self):
+    return self.__item_price
+  
+  def __eq__(self, other):
+    if type(self) == type(other):
+      return self.__item_code == other.__item_code
+    else:
+      raise TypeError("Types not same")
+    
+  def to_hash(self):
+    to_return = ""
+    for char in str(self):
+      to_return += str(ord(char))
+    return int(to_return)
+
+
+  def __hash__(self):
+    return self.to_hash()
+
+  def __str__(self):
+    return f"({self.__item_code}, {self.__item_name}, {self.__item_price})"  # note if grader isn't Storm: f-strings were covered in our section by Prof. Kshetri
+  
+  def sort_by_id_key(self):  # im aware it doesn't necessarily need to be in the class but I'm putting it there for organizatoinal purposes
+    return self.get_code()
 
 
 # manual test from main() method
-def main():     pass
- 
+def main():
+  item_1 = Item("WOND-0001", "Wonder Bread", "2")
+  item_2 = Item("WOND-0002", "Wonder Bagels", 4)
+  item_3 = Item("WOND-0002", "Wonder Bagels New and Improved", 5)
+  items_sold = set()
+  items_sold.add(item_1)
+  items_sold.add(item_2)
+  items_sold.add(item_3)
+  for item in items_sold:  # testing __str__
+    print(item)
+  print(item_2 == item_3)  # Testing equality by item code
+  print(sorted(items_sold, key=Item.sort_by_id_key))  # Testing sort
 
 if __name__ == "__main__":    main()
