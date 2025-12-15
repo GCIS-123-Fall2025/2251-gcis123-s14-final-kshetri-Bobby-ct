f4890087f75bde52fc30886b3bc0e8db85a26b22
Bobby Genova (RIT Student) <bsg1878@rit.edu>
2025-12-15 09:28:53 -0500

    tab settings are screwed up on this machine

--- shoppers.py
+++ shoppers.py
@@ -20,41 +20,41 @@ Write down the manual test by creating at least two items.
 class Item:
     __slots__ = ['__item_code', '__item_name', '__item_price'] 
     def __init__(self, code, name, price):
-      self.__item_code = code
-      self.__item_name = name
-      self.__item_price = int(price)  # Really should be float, but the example states simply "24", not "24.0" or "24.00", so using int to match that
+        self.__item_code = code
+        self.__item_name = name
+        self.__item_price = int(price)  # Really should be float, but the example states simply "24", not "24.0" or "24.00", so using int to match that
 
     # Accessors
     def get_code(self):
-      return self.__item_code
+        return self.__item_code
     def get_name(self):
-      return self.__item_name
+        return self.__item_name
     def get_price(self):
-      return self.__item_price
+        return self.__item_price
     
     def __eq__(self, other):
-      if type(self) == type(other):
-        return self.__item_code == other.__item_code
-      else:
-        raise TypeError("Types not same")
+        if type(self) == type(other):
+            return self.__item_code == other.__item_code
+        else:
+            raise TypeError("Types not same")
       
     def to_hash(self):
-      to_return = ""
-      for char in str(self):
-        to_return += str(ord(char))
-      return int(to_return)
+        to_return = ""
+        for char in str(self):
+            to_return += str(ord(char))
+        return int(to_return)
 
     def __hash__(self):  # We didn't cover how to do this in lecture, __hash__ was always given to us.  Theroetically the method I implemented should always be unique and therefore work as a usable hash
-      return self.to_hash()
+        return self.to_hash()
 
     def __repr__(self):
-      return f"{self.__item_code}"  # instructions don't mention what to include in the sorted representation so I'm assuming just item code
+        return f"{self.__item_code}"  # instructions don't mention what to include in the sorted representation so I'm assuming just item code
 
     def __str__(self):
-      return f"({self.__item_code}, {self.__item_name}, {self.__item_price})"  # note if grader isn't Storm: f-strings were covered in our section by Prof. Kshetri
+        return f"({self.__item_code}, {self.__item_name}, {self.__item_price})"  # note if grader isn't Storm: f-strings were covered in our section by Prof. Kshetri
     
     def sort_by_id_key(self):  # im aware it doesn't necessarily need to be in the class but I'm putting it there for organizatoinal purposes
-      return self.get_code()
+        return self.get_code()
 
 
 # manual test from main() method
@@ -67,7 +67,7 @@ def main():
   items_sold.add(item_2)
   items_sold.add(item_3)
   for item in items_sold:  # testing __str__
-    print(item)
+      print(item)
   print(item_2 == item_3)  # Testing equality by item code
   print(sorted(items_sold, key=Item.sort_by_id_key))  # Testing sort
 
